10da03ea364e999c68dafcdd16883bfc6149e5e0
Tejkumar <mailtejkumar@rediffmail.com>
2020-04-11 18:38:48 +0530

    Initial Commit

---
+++ src/main/java/tgk/Hello.java
@@ -0,0 +1,12 @@
+package tgk;
+
+public class Hello
+{
+	public void greet() 
+	{
+		System.out.println("Hi...");
+		System.out.println("How are you...");
+		
+		System.out.println("Would you like to have coffee or tea ? ");
+	}
+}
---
+++ src/test/java/testtgk/TestHello.java
@@ -0,0 +1,12 @@
+package testtgk;
+
+import tgk.Hello;
+
+public class TestHello 
+{
+	public static void main(String[] args) 
+	{
+		Hello h1 = new Hello();
+		h1.greet();
+	}
+}
